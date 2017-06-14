9d8e342e102d669a86c6e1c66db3f7c66b1b3aa6
heineiuo <heineiuo@gmail.com>
2017-06-14 22:25:30 +0800

    Publish

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import isPlainObject from 'lodash/isPlainObject'
 export default (...styleDefinitions) => {
   const plainStyles = [{}].concat(styleDefinitions).reduce((left, right) => {
     return Object.assign({}, left, isPlainObject(right) ? 
-      right.hasOwnProperty('_definition') ? right.__definition : right : {}
+      right.hasOwnProperty('_definition') ? right._definition : right : {}
     )
   })
-  const style = Object.assign({}, plainStyles)
-  return prefixAll(style)
+  return prefixAll(plainStyles)
 }
